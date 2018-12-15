d32a7bf8ea31ae11003a65fcd37dabaa4deee163
Young Oh <34850791+younggeun0@users.noreply.github.com>
2018-12-15 20:04:41 +0900

    Swing을 이용한 추가구현

---
+++ src/swing_evt/BMIEvt.java
@@ -0,0 +1,102 @@
+package swing_evt;
+
+import java.awt.Transparency;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+import swing_view.BMIView;
+
+public class BMIEvt implements ActionListener {
+
+	private BMIView bv;
+
+	public BMIEvt(BMIView bv) {
+		this.bv = bv;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == bv.getJbCal()) {
+			
+			// 키가 입력이 안됐다면 경고창을 띄우고 키칸으로 이동
+			if (bv.getJtfHeight().getText().isEmpty()) {
+				
+				JOptionPane.showMessageDialog(bv, "키(cm)를 입력해주세요.", "", JOptionPane.WARNING_MESSAGE);
+				bv.getJtfHeight().requestFocus();
+				return;
+			} else {
+				try {
+					// 숫자인지 검증
+					Integer.parseInt(bv.getJtfHeight().getText());
+					
+				} catch (NumberFormatException nfe) {
+					// 숫자가 아니면 경고
+					JOptionPane.showMessageDialog(bv, "키(cm)는 숫자만 입력가능합니다.", "", JOptionPane.WARNING_MESSAGE);
+					// 숫자라면 키 TextField로 Focus
+					bv.getJtfHeight().requestFocus();
+					return;
+				}
+			}
+
+			// 몸무게가 입력이 안됐다면 경고창을 띄우고 몸무게칸으로 이동
+			if (bv.getJtfWeight().getText().isEmpty()) {
+				JOptionPane.showMessageDialog(bv, "몸무게(kg)를 입력해주세요.", "", JOptionPane.WARNING_MESSAGE);
+				bv.getJtfWeight().requestFocus();
+				return;
+			}
+			
+			// 두개가 모두 비어있지 않다면
+			if (!(bv.getJtfHeight().getText().isEmpty() && bv.getJtfWeight().getText().isEmpty())) {
+				try {
+					// 입력된 값이 모두 숫자인지 검증
+					Integer.parseInt(bv.getJtfHeight().getText());
+					Integer.parseInt(bv.getJtfWeight().getText());
+					
+					// 숫자라면 메소드 호출
+					bmiCal();
+					
+				} catch (NumberFormatException nfe) {
+					// 숫자가 아니면 숫자가 아닌 칸이 무엇인지 알려주는 경고
+					JOptionPane.showMessageDialog(bv, "몸무게(kg)는 숫자만 입력가능합니다.", "", JOptionPane.WARNING_MESSAGE);
+					bv.getJtfWeight().requestFocus();
+					return;
+				}
+			}
+		}
+		if (e.getSource() == bv.getJbExit()) {
+			bv.dispose();
+		}
+	}
+
+	public void bmiCal() {
+		double height = 0, weight = 0;
+
+		// height가 비었거나 weight가 비어있지 않다면.
+		if (!(bv.getJtfHeight().getText().isEmpty() || bv.getJtfWeight().getText().isEmpty())) {
+
+			height = Double.valueOf(bv.getJtfHeight().getText()) / 100;
+			weight = Double.valueOf(bv.getJtfWeight().getText());
+
+			double bmiResult = 0;
+			String textResult = null;
+
+			bmiResult = weight / (height * height);
+
+			if (bmiResult >= 30) {
+				textResult = "고도비만입니다.";
+			} else if (bmiResult >= 25 && bmiResult < 30) {
+				textResult = "비만입니다.";
+			} else if (bmiResult >= 23 && bmiResult < 25) {
+				textResult = "과체중입니다.";
+			} else if (bmiResult >= 18.5 && bmiResult < 23) {
+				textResult = "정상입니다.";
+			} else if (bmiResult < 18.5) {
+				textResult = "저체중입니다.";
+			}
+
+			JOptionPane.showMessageDialog(bv, textResult, "결과", JOptionPane.INFORMATION_MESSAGE);
+		}
+	}
+}
--- src/swing_view/BMIView.java
+++ src/swing_view/BMIView.java
@@ -1,24 +1,26 @@
 package swing_view;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
 
-import wing_evt.BMIEvt;
+import swing_evt.BMIEvt;
 
 public class BMIView extends JFrame {
 
 	private JTextField jtfHeight, jtfWeight;
 	private JButton jbCal, jbExit;
 	private JTextArea jtaInfo;
-	private JLabel jlInfo;
 	
 	public BMIView() {
 		
 		super("BMI Checker");
-		jlInfo = new JLabel("BMI란?");
 		JLabel jlHeight = new JLabel("키(cm) : ");
 		JLabel jlWeight = new JLabel("몸무게(kg) : ");
 		jtaInfo = new JTextArea(
@@ -26,19 +28,22 @@ public class BMIView extends JFrame {
 				+ "근거한방식으로 BMI지수= 몸무게(kg) ÷ (신장(m) × 신장(m))입니다. "
 				+ "산출된 값이 18.5 이하면 저체중, 18.5~23은 정상, 23~25는 과체중, "
 				+ "25~30은 비만, 30이상은 고도비만으로 나누어집니다.", 10, 10);
+		TitledBorder tbTa = new TitledBorder("BMI란?"); 
+		jtaInfo.setBorder(tbTa);
+		
 		jtaInfo.setEditable(false);
 		jtaInfo.setLineWrap(true);
 		
-		jbCal = new JButton("계산");
-		jbExit = new JButton("종료");
+		ImageIcon iiCal = new ImageIcon("D:/git/repositories/toyProjectBMI/UI/jbCal.png");
+		ImageIcon iiExit = new ImageIcon("D:/git/repositories/toyProjectBMI/UI/jbClose.png");
+		
+		jbCal = new JButton("계산"/*, iiCal*/);
+		jbExit = new JButton("종료"/*, iiExit*/);
 		
 		jtfHeight = new JTextField();
 		jtfWeight = new JTextField();
 		
-		jlInfo.setBounds(15,20, 45, 20);
-		add(jlInfo);
-		
-		jtaInfo.setBounds(15,40,320, 90);
+		jtaInfo.setBounds(15,20,315, 120);
 		add(jtaInfo);
 		
 		jlHeight.setBounds(36, 160, 50, 20);
@@ -85,8 +90,5 @@ public class BMIView extends JFrame {
 	}
 	public JTextArea getJtaInfo() {
 		return jtaInfo;
-	}
-	public JLabel getJlInfo() {
-		return jlInfo;
 	}
 }
