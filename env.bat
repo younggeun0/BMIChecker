1231bca230da75412898d858b3743f3386304c39
younggeun0 <34850791+younggeun0@users.noreply.github.com>
2019-03-12 17:09:56 +0900

    Update RunnableJar, bat file, demonstration clip

---
+++ env.bat
@@ -0,0 +1,6 @@
+set dev_home=C:\dev
+set java_home=%dev_home%\Java\jdk1.8.0_191
+
+set path=%java_home%\bin;
+
+set classpath=.;%dev_home%\1949\03.°³¹ß\drivers\ojdbc6.jar;
---
+++ run_bmi.bat
@@ -0,0 +1,3 @@
+call env.bat
+
+java -jar bmi.jar
